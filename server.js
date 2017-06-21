4f69e0502889623e762669dd3f2cfa5841f1b6c4
Abose U <bexis18@gmail.com>
2017-06-21 14:06:12 +0000

    add redirect route and mlab mongo connection for heroku deployment

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 
 //connect to the database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/urlShortener");
+var mlabUrl = process.env.MONGOLAB_URI + "url-shortener"; //get mlab url(credentials) from environment variables
+mongoose.Promise = global.Promise;
+mongoose.connect( mlabUrl || "mongodb://localhost/urlShortener"); /*connect to mongodb with mongoose
+either on mlab cloud remote database or locally installed mongodb*/
 
 var connection = mongoose.connection;
 connection.on('error', console.error.bind(console, 'connection error:'))
@@ -20,8 +23,7 @@ connection.on('open', function(){
 
 app.get("/new/:id(*)", function(request, response){
     
-    var originalUrl = request.params.id;
-    
+    var originalUrl = request.params.id; //url user typed in
     
     //check if it's a valid url
     if(!validator.isURL(originalUrl)){
@@ -34,33 +36,54 @@ app.get("/new/:id(*)", function(request, response){
         originalUrl : originalUrl,
         shortenedUrl : shortCode
     });
-    console.log(urlInstance);
     
-    //return back original url and shortened form
-    response.send({
-        originalUrl : originalUrl,
-        shortenedUrl : shortCode
-    });
-    // //store both shortened and original url
-    // var dbUrl = "mongodb://localhost:27017/urlShortener";
-    // mongoose.connect(dbUrl);
-    // var db = mongoose.connection;
-    
-    // db.on('error', console.error.bind(console , "connection error:"));
-    // db.once('open', function(){
-    //     urlPairModel.save(function(err,data){
-    //         if(err){
-    //             throw err;
-    //         }
-    //         else{
-    //         db.close(); 
-    //         response.send("saved in the database");
-    //         }
-    //     })
-    // })
+    //save urlPair data instance in the database
+    urlInstance.save(function(err,data){
+        if(err){
+            console.log(err);
+            response.send("Error saving to the database");
+        }
+        else{
+            //return back to the user only the original url and shortened form
+            var appUrl = 'https://bexis-url-shortener.herokuapp.com/';
+            response.send({
+            originalUrl : originalUrl,
+            shortenedUrl : appUrl + shortCode
+            });
+        }
+    })
    
     
 });
+
+app.get("/:id", function(request, response){
+    //get users shortcode
+    var shortCode = request.params.id;
+    //find document with this shortcode in the database collection urlpairmodel
+    try{
+        urlPairModel.findOne({'shortenedUrl':shortCode}, function(err, doc){
+            if(err){
+                throw err;
+            }
+            else{
+                //check if docs original url does not have http in it
+                var check = doc.originalUrl.substring(0,4);
+                //add http if it does not have
+                if(check !== "http"){
+                    var urlToVisit = "http://" + doc.originalUrl;
+                    response.redirect(urlToVisit);
+                }
+                //it has http, so:
+                //redirect user to appropriate url representation of the shortcode
+                response.redirect(doc.originalUrl);
+            }
+        })
+    }
+    catch(err){
+         response.send("cannot find your shortUrl in database. Please add it" +
+            " as new url /new/your_url_here");
+    }
+})
 
 app.listen(port, function(){
     console.log("app is listening on port");
