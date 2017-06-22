09a57e089225f802acfd5e231c6613a54cfbcf53
Abose U <bexis18@gmail.com>
2017-06-22 15:32:58 +0000

    fix mongodb mlab issues

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'public')));
 
 //connect to the database
-var mlabUrl = process.env.MONGOLAB_URI; //get mlab url(credentials) from environment variables
+var mlabUrl = process.env.MONGOLAB_URI + "url-shortener"; //get mlab url(credentials) from environment variables and url-shortner is dbname
 mongoose.Promise = global.Promise;
-mongoose.connect( mlabUrl || "mongodb://localhost/urlShortener"); /*connect to mongodb with mongoose
+mongoose.connect(mlabUrl || "mongodb://localhost:27017/urlShortener"); /*connect to mongodb(on mlab, no locally) with mongoose
 either on mlab cloud remote database or locally installed mongodb*/
 
 var connection = mongoose.connection;
